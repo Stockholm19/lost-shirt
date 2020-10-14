78f81b488bc2460edc3b90cb6402e258e8b7394d
Роман Пшеничников <roman.pshenichnikov@MacBook-Air-Roman.local>
2020-10-14 21:02:31 +0300

    37.1 removed extra code

--- scripts/fade-slider.js
+++ scripts/fade-slider.js
@@ -1,38 +1,4 @@
 (function () {
-
-   // const slides = document.querySelectorAll('.fade-slider__item');
-   // const activeClass = "fade-slider__item--visible";
-   // let index = 0;
-   // console.log(slides[4])
-
-   // setInterval(function () {
-   //    slides[index].classList.remove('activeClass')
-   //    index++;
-   //    slides.classList.add(activeClass);
-
-   // }, 1000);
-
-   // const slides = document.querySelectorAll('.fade-slider__item');
-   // let index = 0;
-   // let activeClass = "fade-slider__item--visible";
-
-   // console.log(slides)
-
-   // setInterval(function () {
-   //    slides[index].classList.remove(activeClass)
-   //    index++;
-
-   //    if (index + 1 > slides.length) {
-   //       index = 0;
-   //    }
-
-   //    slides[index].classList.add(activeClass);
-
-
-   // }, 1000)
-
-
-
    const slides = document.querySelectorAll('.fade-slider__item');
    const activeClass = "fade-slider__item--visible";
    let index = 0;
@@ -44,6 +10,4 @@
       }
       slides[index].classList.add(activeClass);
    }, 5000);
-
-
 }())
